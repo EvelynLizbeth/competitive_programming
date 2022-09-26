b54b7a20f113f6dfb8e0b600f4e2ed01c55ab2da
Evelyn <82476361+EvelynLizbeth@users.noreply.github.com>
2022-09-25 23:43:59 -0500

    Add files via upload

---
+++ SET_1/10071 - Back to High School Physics.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int velocidad, tiempo;
+    while (cin >> velocidad >> tiempo){
+        cout << (velocidad*(2*tiempo)) << '\n';
+    }
+}
+
